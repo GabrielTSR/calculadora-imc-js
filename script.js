65052b3b7e38952b9931741a1d0dc4b5ae54d6df
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-08-10 17:48:32 -0300

    calculos funcionando

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const nome = document.getElementById("nome")
-const peso = document.getElementById("peso")
-const altura = document.getElementById("altura")
-const imc = calcularImc(peso.value, altura.value)
-const status = exibirResultado(imc)
+const buttonCalcular = document.querySelector("#calcularButton")
+buttonCalcular.addEventListener("click", calcularImc);
 
-resultado.value = status
+function calcularImc() {
 
-function exibirResultado(imc, nome) {
+    const nome = document.getElementById("nome")
+    let peso = document.getElementById("peso")
+    peso.value = peso.value.replace(',', '.')
+    let altura = document.getElementById("altura")
+    altura.value = altura.value.replace(',', '.')
+    const imc = (peso.value / Math.pow(altura.value, 2)).toFixed(2)
+
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML = exibirResultado(imc)
+
+    exibirResultado(imc)
+
+
+
+}
+
+function exibirResultado(imc) {
     let status
 
-    if (imc < 18.5) {
-        status = 'Abaixo do peso'
-    } else if (imc < 24.9) {
-        status = 'Peso ideal'
-    } else if (imc < 29.9) {
-        status = 'Acima do peso'
-    } else if (imc < 34.9) {
-        status = 'Obesidade grau I'
-    } else if (imc < 39.9) {
-        status = 'Obesidade grau II'
-    } else {
-        status = 'Obesidade grau III'
+    if (imc < 18.5) {status = 'Abaixo do peso'} 
+    else if (imc <= 24.9) {status = 'Peso ideal!'} 
+    else if (imc <= 29.9) {status = 'Acima do peso'} 
+    else if (imc <= 34.9) {status = 'Obesidade grau I'} 
+    else if (imc <= 39.9) {status = 'Obesidade grau II'} 
+    else if (imc <= 40) {status = 'Obesidade grau III'} 
+    else {
+        return alert("ATENÇÃO: OS CAMPOS NÃO FORAM PREENCHIDOS CORRETAMENTE. CERTIFIQUE-SE DE COLOCAR APENAS NÚMEROS NOS CAMPOS DE \"ALTURA\" E \"PESO\"")
     }
-    status = nome + "IMC = " +
+    status = nome.value + ", seu IMC é igual a " +
         imc + ", " + status
-    alert(status)
+
     return status
 }
-
-function calcularImc(peso, altura) {
-
-    const imc = peso / Math.pow(altura, 2)
-    return imc
-}
-
-calcularButton.addEventListener("click", exibirResultado(imc, nome))
